9136f947374e2548e823c5eb40daadd2f5bb4f7c
Jose C Morgado Vargas <josecruzmv@outlook.com>
2021-12-30 12:57:22 -0600

    Made function to print games board

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 package connectfour
 
 const val defaultBoardDimension = "6 X 7"
+const val defaultRows = "6"
+const val defaultColumns = "7"
 fun main() {
     var boardDimensions= ""
     val range = 5..9
     val regex = Regex("\\s*?\\d\\d?\\s*?x\\s*?\\d\\d?\\s*?")
+    var auxBoard: MutableList<String> = mutableListOf()
 
     println("Connect Four")
     println("First player's name:")
@@ -23,7 +26,7 @@ fun main() {
             if (boardDimensions.lowercase().matches(regex)) {
                 //Divide boardDimensions in rows and columns using x character
                 //replace("\\s".toRegex(), "") to remove all whiteSpaces no matter if they are space or tab
-                val auxBoard = boardDimensions.lowercase().replace("\\s".toRegex(), "").split("x")
+                auxBoard = boardDimensions.lowercase().replace("\\s".toRegex(), "").split("x").toMutableList()
                 //Checking that rows then columns are in range
                 if (auxBoard[0].toInt() in range) {
                     if (auxBoard[1].toInt() in range) {
@@ -40,9 +43,42 @@ fun main() {
             }
         } else {
             boardDimensions = defaultBoardDimension
+            auxBoard.add(defaultRows)
+            auxBoard.add(defaultColumns)
             wrongDimension = false
         }
     }
     //Output message
     println("$firstPlayer VS $secondPlayer\n$boardDimensions board")
+
+    //Printing game's board
+    printGameBoard(auxBoard)
+}
+
+//Get the auxBoard variable where auxBoard[0] = rows and auxBoard[1] = columns
+fun printGameBoard(boardDimensions: MutableList<String>) {
+    var board = ""
+    //Rows plus numbers row
+    val rows = boardDimensions[0].toInt() + 1
+    val columns = boardDimensions[1].toInt()
+    //iterating in rows
+    for (i in 0..rows) {
+        //Iterating in columns
+        for (j in 1..columns) {
+            //if i counter is in the last row
+            if (i == rows) {
+                board += when(j) {
+                    1 -> "╚"
+                    columns -> "═╩═╝"
+                    else -> "═╩"
+                }
+            } else {
+                board += if (i == 0) " $j" else "║ "
+                board += if (j == columns && i != 0) "║" else ""
+            }
+        }
+        board += "\n"
+    }
+
+    print(board)
 }
